63628e22c4c1cface5b2abd39116dc2d0dea7479
PChunark <puttipongchunark@gmail.com>
2023-12-03 20:05:57 +0700

    Add a pipeline to a driver.bash

--- driver.bash
+++ driver.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Get all the daily data from all weather stations and 
+# generate list of stations
 code/get_ghcnd_data.bash ghcnd_all.tar.gz
+code/get_ghcnd_all_files.bash
+
+# Get listing of types of data found at each weather station
 code/get_ghcnd_data.bash ghcnd-inventory.txt
+
+# Get metadata for each weather station
 code/get_ghcnd_data.bash ghcnd-stations.txt
